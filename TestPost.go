c75aa1add04733cb62c22ba99fe2a063efba8cc9
fdxwy <65343668+fdxwy@users.noreply.github.com>
2020-07-03 16:08:14 +0800

    Update TestPost.go

--- TestPost.go
+++ TestPost.go
@@ -1,15 +1,20 @@
 package main
 
 import (
-	"fmt"
-	"net/http"
+"fmt"
+"net/http"
 )
 
 func indexHandler(w http.ResponseWriter, r *http.Request) {
-	body := r.PostFormValue("topic")
-	fmt.Fprintf(w,body)
+	method :=r.Method
+	body := r.Body
+	header := r.Header
+	topic := r.PostFormValue("topic")
+	fmt.Fprintln(w, method)
+	fmt.Fprintln(w, body)
+	fmt.Fprintln(w, header)
+	fmt.Fprintf(w,topic)
 }
-
 func main() {
 	http.HandleFunc("/", indexHandler)
 	http.ListenAndServe(":8803", nil)
